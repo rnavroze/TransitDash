0d040b05e977a9b22c95b4379fa73ede0eb02bca
raihan@raitech.org <Raihan>
2023-02-21 04:49:58 +0000

    Configure deployment path

--- vite.config.js
+++ vite.config.js
@@ -28,6 +28,7 @@ export default defineConfig({
             ]
         }
     })],
+    base: '/transitdash',
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
