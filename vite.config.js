9683acca8796809c1b1e5a39162945630d2ee187
raihan@raitech.org <Raihan>
2023-02-21 04:44:50 +0000

    Add PWA support

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,36 @@
-import { fileURLToPath, URL } from "node:url";
+import {fileURLToPath, URL} from "node:url";
 
-import { defineConfig } from "vite";
+import {defineConfig} from "vite";
 import vue from "@vitejs/plugin-vue";
+import {VitePWA} from 'vite-plugin-pwa';
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [vue(), VitePWA({
+        registerType: 'autoUpdate',
+        manifest: {
+            name: "TransitDash",
+            short_name: "TransitDash",
+            description: "Transit Dashboard",
+            theme_color: "#000055",
+            icons: [
+                {
+                    src: 'android-chrome-192x192.png',
+                    sizes: '192x192',
+                    type: 'image/png',
+                },
+                {
+                    src: 'android-chrome-512x512.png',
+                    sizes: '512x512',
+                    type: 'image/png',
+                }
+            ]
+        }
+    })],
+    resolve: {
+        alias: {
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
+        },
     },
-  },
 });
